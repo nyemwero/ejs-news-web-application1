0e0fb525a22994e67031b617e21af7432e9c4825
Gerald Nyemwero <gnaswoswere@gmail.com>
2025-01-24 02:50:19 +0200

    updated search button

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,6 +1,7 @@
 // src/routes/home.ts
 import { Router, Request, Response } from 'express';
 import * as ArticleController from '../controllers/articleController';
+import * as Article from '../models/articleModel'
 
 const router = Router();
 
@@ -9,6 +10,27 @@ router.get('/', ArticleController.getAllArticles, (req: Request, res: Response) 
   res.render('home', { title: 'Home Page'});
 });
 
+// Search route
+router.get('/search', async (req: Request, res: Response) => {
+  try {
+    // Fetch all articles
+    const articles = await Article.fetchAllArticles();
 
+
+
+    // Call the filterArticles method with the query
+    const filteredArticles = ArticleController.filterArticles(req, articles);
+
+    console.log("Filtered articles:", filteredArticles);
+
+    // Render the admin page with filtered articles
+    res.render('home', { title: 'Home Page', articles: filteredArticles });
+  } catch (error) {
+    console.error('Error during search route:', error);
+    res.status(500).send('Error fetching articles');
+  }
+});
 
 export default router;
+
+
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,5 +1,6 @@
 // src/types/express.d.ts
 import * as express from 'express';
+import { Article } from '../models/articleModel';  // Adjust the import based on your Article type
 
 declare global {
   namespace Express {
@@ -8,3 +9,14 @@ declare global {
     }
   }
 }
+
+
+
+declare global {
+  namespace Express {
+    interface Request {
+      articles: Article[];  // Define the type of articles as an array of Article objects
+    }
+  }
+}
+
